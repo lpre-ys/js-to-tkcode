b27e285b5870af397ab0c5d1cd0f13018b60e246
lpre-ys <lpre.yak.shaving@gmail.com>
2017-01-07 20:58:24 +0900

    微修正

---
+++ app.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const JsToTkcode = require('./lib/js-to-tkcode');
+const TkMock = require('./util/tk-mock');
+
+module.exports = {
+  "JsToTkcode": JsToTkcode,
+  "TkMock": TkMock
+};
--- js/app.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-const JsToTkcode = require('./lib/js-to-tkcode');
-
-module.exports = JsToTkcode;
